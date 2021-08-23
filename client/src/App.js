230148872e3b51d20ab170e7aa87469c8418a730
Your Name <you@example.com>
2021-08-23 12:39:18 -0400

    App.js update

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import { Provider } from 'react-redux';
 import store from '.utils/store';
