518afe404cb6e6afaf6ccab9d1ab50d0fc561fe0
Your Name <you@example.com>
2021-08-23 09:09:44 -0400

    Cart Index.js Updates

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,4 +1,6 @@
+// Creating a Redux Store, which holds the state of the application.
 import { createStore } from 'redux';
+// Importing the reducer file itself.
 import reducers from './reducers';
-
+// Exporting the reducers file to the createStore function.
 export default createStore(reducers);
